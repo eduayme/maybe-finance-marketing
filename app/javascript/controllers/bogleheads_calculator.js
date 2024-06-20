334d182015d10db1c4f360278be8e86295242ebf
Eduard Aymerich <eduardaymerich@gmail.com>
2024-06-20 21:03:36 +0000

    feat add initial amount

--- app/javascript/controllers/bogleheads_calculator.js
+++ app/javascript/controllers/bogleheads_calculator.js
@@ -1,5 +1,28 @@
 import { Controller } from "@hotwired/stimulus";
 import TemplateRenderer from "helpers/template_renderer";
+
 // Connects to data-controller="bogleheads-calculator"
 export default class extends Controller {
+  static targets = ["resultsTemplate", "resultsContainer"];
+
+  calculate(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const parseFormData = key => parseFloat(formData.get(key).replace(/[^0-9.-]+/g, ''));
+
+    const monthlyExpenses = parseFormData("invested_amount");
+
+    this.#renderResults({ totalValue: monthlyExpenses });
+    return;
+  }
+
+  #renderResults(data) {
+    const resultsElement = this.resultsRenderer.render(data);
+    this.resultsContainerTarget.innerHTML = "";
+    this.resultsContainerTarget.appendChild(resultsElement);
+  }
+
+  get resultsRenderer() {
+    return new TemplateRenderer(this.resultsTemplateTarget);
+  }
 }
